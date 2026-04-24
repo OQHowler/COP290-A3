af2dce692e3a2469ef65ccb30494f48ec87fb78e
Shoubhit Wandile <cs5240164@iitd.ac.in>
2026-04-24 05:17:45 +0000

    initial commit

---
+++ CMakeFiles/leveldb_tests.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/shoubhit/COP290A3/leveldb/db/autocompact_test.cc" "CMakeFiles/leveldb_tests.dir/db/autocompact_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/autocompact_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/corruption_test.cc" "CMakeFiles/leveldb_tests.dir/db/corruption_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/corruption_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/db_test.cc" "CMakeFiles/leveldb_tests.dir/db/db_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/db_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/dbformat_test.cc" "CMakeFiles/leveldb_tests.dir/db/dbformat_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/dbformat_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/filename_test.cc" "CMakeFiles/leveldb_tests.dir/db/filename_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/filename_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/log_test.cc" "CMakeFiles/leveldb_tests.dir/db/log_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/log_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/recovery_test.cc" "CMakeFiles/leveldb_tests.dir/db/recovery_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/recovery_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/skiplist_test.cc" "CMakeFiles/leveldb_tests.dir/db/skiplist_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/skiplist_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/version_edit_test.cc" "CMakeFiles/leveldb_tests.dir/db/version_edit_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/version_edit_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/version_set_test.cc" "CMakeFiles/leveldb_tests.dir/db/version_set_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/version_set_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/db/write_batch_test.cc" "CMakeFiles/leveldb_tests.dir/db/write_batch_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/db/write_batch_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/helpers/memenv/memenv_test.cc" "CMakeFiles/leveldb_tests.dir/helpers/memenv/memenv_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/helpers/memenv/memenv_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/table/filter_block_test.cc" "CMakeFiles/leveldb_tests.dir/table/filter_block_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/table/filter_block_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/table/table_test.cc" "CMakeFiles/leveldb_tests.dir/table/table_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/table/table_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/arena_test.cc" "CMakeFiles/leveldb_tests.dir/util/arena_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/arena_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/bloom_test.cc" "CMakeFiles/leveldb_tests.dir/util/bloom_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/bloom_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/cache_test.cc" "CMakeFiles/leveldb_tests.dir/util/cache_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/cache_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/coding_test.cc" "CMakeFiles/leveldb_tests.dir/util/coding_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/coding_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/crc32c_test.cc" "CMakeFiles/leveldb_tests.dir/util/crc32c_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/crc32c_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/hash_test.cc" "CMakeFiles/leveldb_tests.dir/util/hash_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/hash_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/logging_test.cc" "CMakeFiles/leveldb_tests.dir/util/logging_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/logging_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/no_destructor_test.cc" "CMakeFiles/leveldb_tests.dir/util/no_destructor_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/no_destructor_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/status_test.cc" "CMakeFiles/leveldb_tests.dir/util/status_test.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/status_test.cc.o.d"
+  "/home/shoubhit/COP290A3/leveldb/util/testutil.cc" "CMakeFiles/leveldb_tests.dir/util/testutil.cc.o" "gcc" "CMakeFiles/leveldb_tests.dir/util/testutil.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
